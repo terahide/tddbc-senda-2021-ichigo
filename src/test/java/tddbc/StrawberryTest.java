dc98dcd771ac5859f6bfa37363bd99f907fe8697
terahide27 <terahide27@gmail.com>
2021-03-06 11:24:21 +0900

    refactor change sample to ichigo

--- src/test/java/tddbc/StrawberryTest.java
+++ src/test/java/tddbc/StrawberryTest.java
@@ -10,11 +10,11 @@ import static org.junit.jupiter.api.Assertions.*;
  * * いちご(Strawberry)を生成して品種を取得する
  * * サイズを取得する
  */
-public class SampleTest {
+public class StrawberryTest {
 
     @Test
-    @DisplayName("should return Hello TDD Boot Camp")
-    public void _should_return_Hello_TDD_BootCamp() throws Exception {
+    @DisplayName("いちごを生成するテスト")
+    public void あまおうテスト() throws Exception {
         // Setup
         Strawberry sut = new Strawberry();
         // Exercise
