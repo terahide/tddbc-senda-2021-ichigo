27cd21d212315086096910a962febac1d2cc5353
terahide27 <terahide27@gmail.com>
2021-03-06 14:06:27 +0900

    refactoring いらなくなったテストケース消した

--- src/test/java/tddbc/StrawberryTest.java
+++ src/test/java/tddbc/StrawberryTest.java
@@ -29,37 +29,14 @@ public class StrawberryTest {
         assertEquals(品種, actual);
     }
 
-    @Nested
-    class サイズのテスト {
-        @ParameterizedTest
-        @EnumSource(Size.class)
-        public void サイズのテスト(Size サイズ) throws Exception {
-            // Setup
-            Strawberry sut = new Strawberry(あまおう, サイズ);
-            // Exercise
-            Size actual = sut.getSize();
-            // Verify
-            assertEquals(サイズ, actual);
-        }
-        @Test
-        @DisplayName("S")
-        public void s() throws Exception {
-            // Setup
-            Strawberry sut = new Strawberry(あまおう, S);
-            // Exercise
-            Size actual = sut.getSize();
-            // Verify
-            assertEquals(S, actual);
-        }
-        @Test
-        @DisplayName("M")
-        public void m() throws Exception {
-            // Setup
-            Strawberry sut = new Strawberry(あまおう, M);
-            // Exercise
-            Size actual = sut.getSize();
-            // Verify
-            assertEquals(M, actual);
-        }
+    @ParameterizedTest
+    @EnumSource(Size.class)
+    public void サイズのテスト(Size サイズ) throws Exception {
+        // Setup
+        Strawberry sut = new Strawberry(あまおう, サイズ);
+        // Exercise
+        Size actual = sut.getSize();
+        // Verify
+        assertEquals(サイズ, actual);
     }
 }
