3f3c6e1baeb9f21adfaf63ea076b33f4f2e61c6f
terahide27 <terahide27@gmail.com>
2021-03-06 15:35:38 +0900

    refactoring

--- src/main/java/tddbc/Size.java
+++ src/main/java/tddbc/Size.java
@@ -4,9 +4,7 @@ public enum Size {
     S,M,L,LL;
 
     public static Size toSize(int weight) {
-        if(25 <= weight){
-            return LL;
-        }
+        if(25 <= weight) return LL;
         if(20 <= weight) return L;
         if(10 <= weight) return M;
         return S;
