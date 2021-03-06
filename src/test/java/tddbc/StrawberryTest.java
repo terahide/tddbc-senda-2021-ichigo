c9bf63ea6bed6a9e045b88083a74144819651716
terahide27 <terahide27@gmail.com>
2021-03-06 14:30:09 +0900

    refactoring テストの構造化

--- src/test/java/tddbc/StrawberryTest.java
+++ src/test/java/tddbc/StrawberryTest.java
@@ -24,25 +24,28 @@ import static tddbc.Size.S;
  * [] 1g以上 → S
  */
 public class StrawberryTest {
-    @ParameterizedTest(name="品種の文字列表現のテスト:{0}")
-    @EnumSource(Kind.class)
-    public void 品種の文字列表現のテスト(Kind 品種) throws Exception {
-        // Setup
-        Strawberry sut = new Strawberry(品種,S);
-        // Exercise
-        String actual = sut.toString();
-        // Verify
-        assertEquals(String.format("%s: S", 品種.name()), actual);
-    }
+    @Nested
+    class いちごの文字列表現のテスト{
+        @ParameterizedTest(name="品種の文字列表現のテスト:{0}")
+        @EnumSource(Kind.class)
+        public void 品種の文字列表現のテスト(Kind 品種) throws Exception {
+            // Setup
+            Strawberry sut = new Strawberry(品種,S);
+            // Exercise
+            String actual = sut.toString();
+            // Verify
+            assertEquals(String.format("%s: S", 品種.name()), actual);
+        }
 
-    @ParameterizedTest(name="サイズの文字列表現のテスト:{0}")
-    @EnumSource(Size.class)
-    public void サイズの文字列表現のテスト(Size サイズ) throws Exception {
-        // Setup
-        Strawberry sut = new Strawberry(あまおう, サイズ);
-        // Exercise
-        String actual = sut.toString();
-        // Verify
-        assertEquals(String.format("あまおう: %s", サイズ.name()), actual);
+        @ParameterizedTest(name="サイズの文字列表現のテスト:{0}")
+        @EnumSource(Size.class)
+        public void サイズの文字列表現のテスト(Size サイズ) throws Exception {
+            // Setup
+            Strawberry sut = new Strawberry(あまおう, サイズ);
+            // Exercise
+            String actual = sut.toString();
+            // Verify
+            assertEquals(String.format("あまおう: %s", サイズ.name()), actual);
+        }
     }
 }
